1e7e7b5d6320b55d6bba8078e47a6b10fc7f17d7
Khanh Nguyen <ngpkhanh08@gmail.com>
2026-06-17 10:21:53 +0700

    Fix: Capitalize 'plant.h'

--- src/Plant.cpp
+++ src/Plant.cpp
@@ -1,4 +1,4 @@
-#include "plant.h"
+#include "Plant.h"
 
 Plant::Plant(Resources& r, int x, int y, int hp, int sunCost, std::string name)
     : res(r), m_x(x), m_y(y), m_hp(hp), m_maxHp(hp), m_sunCost(sunCost), m_name(name) {
