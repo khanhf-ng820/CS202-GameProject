06c47ab1a7e235e211ec870b914c2846e97f6020
hoanganhtu0922 <hoanganhtu0922@gmail.com>
2026-07-31 11:04:43 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # Running CMake build commands in the terminal
-cmake -S . -B build -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake -S . -B build "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
 cmake --build build --config Release --parallel 4
