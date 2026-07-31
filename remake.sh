ca66bd93b00f44111c775bfe387974f015d2580f
Khanh Nguyen <ngpkhanh08@gmail.com>
2026-07-31 11:08:16 +0700

    fix: updated the script to recompile the project

--- remake.sh
+++ remake.sh
@@ -1,3 +1,4 @@
-# Run `make -j4` without running `cmake -B build`/`cmake --build build` again
-cd build
-make -j4
+# Old: Run `make -j4` without running `cmake -B build`/`cmake --build build` again
+# cd build
+# make -j4
+cmake --build build --config Release --parallel 4
